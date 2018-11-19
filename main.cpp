f4250f4ef65325343b187e4bde3aa062caaef9c6
MostlyAwesom <egremy.bruno@gmail.com>
2018-11-19 15:24:07 +0100

    Initialisation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <cstdlib>
+
+
+using namespace std;
 
 int main() {
-   std::cout << "Hello" << std::endl;
 
-   return 0;
+   //Déclaration des variables et constantes
+   const int   LETTRE_MIN = 1,
+               LETTRE_MAX = 26,
+               LANCE_MIN = 1,
+               LANCE_MAX = 10;
+
+   int         nbLance,
+               nbCorrect;
+
+   double      tempsMoyen,
+               tempsTotal;
+
+   char        reponseLance,
+               reponseRejouer;
+
+
+   //Présentation programme
+
+   //Boucle pour recommencer
+      //Saisie nombre de lancer ( GESTION ERREUR )
+
+      //Jeu ---- Boucle for avec nbLancer
+                        //Généré aléatoirement une lettre aléatoire miniscule
+                        //Afficher la lettre
+                        //Commence le timer
+                        //Récupéré la réponse
+                        //Pause le timer
+                        //Vérifier réponse et incrémenter le nbCorrect selon
+      //Afficher nbCorrect
+      //Affiche le temps total
+      //Calcul la moyenne du temps pour chaque lancé --------------- Fonction double tempsMoyen(int nbElements, double temps)
+      //affiche le temps moyen
+   //Message pour recommencer" O ou N pour recommencer
+   //Quitter (Message "Le programme se ferme)
+
+
+
+
+   return EXIT_SUCCESS;
 }
