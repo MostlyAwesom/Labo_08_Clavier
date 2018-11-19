8ba86bf5bf093355e5a173d1f525a869372b3b7d
MostlyAwesom <egremy.bruno@gmail.com>
2018-11-19 15:41:15 +0100

    Initialisation

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+void lance(const int& CAR_MIN, const int& CAR_MAX, const int& NB_LANCE, int& nbJuste, int& chrono);
+
 int main() {
 
    //Déclaration des variables et constantes
@@ -21,28 +23,33 @@ int main() {
    char        reponseLance,
                reponseRejouer;
 
-
    //Présentation programme
 
    //Boucle pour recommencer
       //Saisie nombre de lancer ( GESTION ERREUR )
 
       //Jeu ---- Boucle for avec nbLancer
-                        //Généré aléatoirement une lettre aléatoire miniscule
-                        //Afficher la lettre
-                        //Commence le timer
-                        //Récupéré la réponse
-                        //Pause le timer
-                        //Vérifier réponse et incrémenter le nbCorrect selon
+                        //Fonction Lance()
       //Afficher nbCorrect
       //Affiche le temps total
       //Calcul la moyenne du temps pour chaque lancé --------------- Fonction double tempsMoyen(int nbElements, double temps)
       //affiche le temps moyen
    //Message pour recommencer" O ou N pour recommencer
-   //Quitter (Message "Le programme se ferme)
+   //Quitter (Message "Le programme se ferme
 
 
 
 
    return EXIT_SUCCESS;
+}
+
+void lance(const int& CAR_MIN, const int& CAR_MAX, const int& NB_LANCE, int& nbJuste, int& chrono)
+{
+   //Générer lettre
+   //Afficher lettre
+   //Commencer Chrono
+   //Attendre réponse
+   //Vider buffer
+   //Arreter chrono
+   //Verifier reponse et incrément ou non nbJuste
 }
