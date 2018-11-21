c40e74f9544a509634c913535a08b39a3811f68b
MostlyAwesom <egremy.bruno@gmail.com>
2018-11-21 11:25:12 +0100

    Doc chrono

--- chronometre.h
+++ chronometre.h
@@ -3,13 +3,14 @@
 
 /**
  *\brief   Calcul le temps passé en seconde entre les instructions chrono(true) et chrono(false).
- *         Chaque chrono(false) renvoie le temps écoulé depuis le précédent chrono(true).
- *         Après un chrono(true) plusieurs chrono(false) sont utilisables pour donner le temps
- *         écoulé depuis l'instruction chrono(true).
  * @param  active Définit si l'on active le chronometre ou si on regarde le temps écoulé.
  * @return 1. si le chrono commence.
  *         0. si on regarde le temps écoulé sans jamais avoir lancé le chronomètre.
  *         Sinon renvoi le temps écoulé en seconde.
+ *
+ *         Chaque chrono(false) renvoie le temps écoulé depuis le précédent chrono(true).
+ *         Après un chrono(true) plusieurs chrono(false) sont utilisables pour donner le temps
+ *         écoulé depuis l'instruction chrono(true).
  */
 double chrono(const bool& active);
 
