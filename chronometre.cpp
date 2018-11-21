5d0ccbf2daa39add9e7982ee007606efec6b7336
MostlyAwesom <egremy.bruno@gmail.com>
2018-11-21 11:23:30 +0100

    Doc chrono

--- chronometre.cpp
+++ chronometre.cpp
@@ -2,14 +2,20 @@
 #include <ctime>         //time, difftime
 
 double chrono(const bool& active){
+   // Initialisation du temps de initial de référence en cas de mauvaise utilisation
    static time_t tempsInitial = time(nullptr);
+
    time_t tempsFinal;
 
    if(active) {
+      // Définit le temps au lancement du chronomètre
       tempsInitial = time(nullptr);
-      return 0.;
+      // Retourne 1. car le chronomètre se lance
+      return 1.;
    } else {
+      // Définit le temps à l'arrêt du chronomètre
       tempsFinal = time(nullptr);
+      // Retourne la différence de temps entre le lancement et l'arrêt du chronomètre
       return difftime(tempsFinal,tempsInitial);
    }
 }
--- generationAleatoire.h
+++ generationAleatoire.h
@@ -2,9 +2,9 @@
 #define LABO_08_CLAVIER_GENERATIONALEATOIRE_H
 
 /**
- * \brief Génère un nombre aléatoire selon des bornes.
- * @param min Nombre minimum.
- * @param max Nombre maximum.
+ * \brief  Génère un nombre aléatoire selon des bornes.
+ * @param  min Nombre minimum.
+ * @param  max Nombre maximum.
  * @return Retourne un nombre aléatoire selon les bornes.
  */
 int nombreAleatoire(const int& min, const int& max);
